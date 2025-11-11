80442dea17c35ce55567047e1284afbc641b18a7
Arman Dezfuli <armandezfuli@proton.me>
2025-11-11 10:12:55 +0330

    feat(quiz): implement quiz page UI and improve Button component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -2,12 +2,13 @@ import clsx from "clsx"
 
 type ButtonType = {
     variant: "secondary" | "primary"
-    text: string
+    text?: string
+    icon?: string
 }
 
-function Button({ variant, text }: ButtonType) {
+function Button({ variant, text, icon }: ButtonType) {
     const base_styles =
-        "w-full h-[58px] flex justify-center items-center rounded-primary text-[32px] py-2.5 px-5 cursor-pointer transition-all duration-300 ease-out"
+        "flex justify-center items-center rounded-primary text-[32px] py-2.5 px-5 cursor-pointer transition-all duration-300 ease-out"
     const primaryStyle =
         "border border-text-secondary text-text-secondary bg-primary/30 focus:bg-primary hover:bg-primary/70"
     const secondaryStyle =
@@ -20,6 +21,7 @@ function Button({ variant, text }: ButtonType) {
                 variant === "primary" && primaryStyle,
                 variant === "secondary" && secondaryStyle
             )}>
+            {icon && <img src={icon} />}
             {text}
         </button>
     )
--- app/routes/quiz.tsx
+++ app/routes/quiz.tsx
@@ -1,18 +1,39 @@
-
+import Button from "~/components/button"
+import LeftArrow from "../assets/icons/Left arrow.svg"
+import RightArrow from "../assets/icons/Right arrow.svg"
 
 export function meta() {
     return [
-      { title: "Quiz App | Quiz" },
-      { name: "description", content: "Answer fun quizzes and challenge yourself!" },
-    ];
-  }
-  
-
+        { title: "Quiz App | Quiz" },
+        { name: "description", content: "Answer fun quizzes and challenge yourself!" },
+    ]
+}
 
 export default function Quiz() {
     return (
-        <>
-            <h1>Quiz</h1>
-        </>
+        <main className="p-5 md:p-10 flex flex-col items-center">
+            <div className="w-full flex items-center text-text-secondary gap-6">
+                <Button variant="secondary" icon={LeftArrow} />
+                <span className="text-2xl md:text-4xl">Oxford Word Skills Elementary</span>
+            </div>
+            <div className="mt-16 bg-surface rounded-primary p-4 md:p-7.5">
+                <div className="text-3xl md:text-[40px]">
+                    <h1>I usually drink a cup of coffee in the __________.</h1>
+                </div>
+                <div className="flex flex-col gap-y-8 mt-[76px]">
+                    <Button variant="primary" text="morning" />
+                    <Button variant="primary" text="night" />
+                    <Button variant="primary" text="lunch" />
+                    <Button variant="primary" text="afternoon" />
+                </div>
+                <div className="flex justify-center md:justify-end  mt-[76px]">
+                    <div className="flex gap-x-5.5 items-center">
+                        <Button variant="primary" icon={LeftArrow} />
+                        <span className="text-2xl sm:text-[32px]">1 of 10</span>
+                        <Button variant="primary" icon={RightArrow} />
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
